cb852377cc7f27bca9ea628991e33ac07c3db373
Shravan Tomar <shravan@Shravans-MacBook-Air.local>
2024-06-03 23:06:13 +0530

    feat: variable declaration

--- FirstDemo.py
+++ FirstDemo.py
@@ -1,2 +1,16 @@
 print("hello")
 print("second line")
+
+# variable declaration
+
+a = 5
+print(a)
+
+Str = "Hello World"
+
+# same line multiple variable declaration
+
+b, c, d = 6, 7.7, "Great"
+print(b)
+print(c)
+print(d)
