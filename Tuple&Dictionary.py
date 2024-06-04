1567d8af5f433ea5ed08313ff1772ef20f488303
Shravan Tomar <shravan@Shravans-MacBook-Air.local>
2024-06-05 00:22:39 +0530

    feat: tuple & dictionary data type

---
+++ Tuple&Dictionary.py
@@ -0,0 +1,6 @@
+val = (1, 2, 3, "Shravan", 8.214) # this is a tuple data type which is immutable
+print(val[3])
+
+val2 = {1:"Shravan", "b":4.88, "name":"Tomar"}
+print(val2[1])
+print(val2["name"])
