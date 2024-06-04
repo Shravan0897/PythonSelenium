7bf6ad224099e65a93860fa582819c4845375e6e
Shravan Tomar <shravan@Shravans-MacBook-Air.local>
2024-06-04 16:47:51 +0530

    feat: list data type

--- FirstDemo.py
+++ FirstDemo.py
@@ -14,3 +14,12 @@ b, c, d = 6, 7.7, "Great"
 print(b)
 print(c)
 print(d)
+
+# to concatenate two different type of variables
+print("{} {}".format("Value is", b))
+print("Float value is ", c)
+
+# to know the type of the variable
+print(type(b))
+print(type(c))
+print(type(d))
